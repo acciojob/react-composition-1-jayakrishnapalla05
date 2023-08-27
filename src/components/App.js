d5c0f254aa3f665b1fe9c74addf9375576a2f9a5
jayakrishnapalla05 <jayakrishnapalla05@gmail.com>
2023-08-27 14:10:46 +0530

    submitting code-Sun Aug 27 2023 14:10:45 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,29 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
-import Tabs from "./Tabs";
 
 
-const tabs = [{title:"Tab 1" , content: "Tab 1"}, 
-{title:"Tab 2" , content: "Tab 2"}, 
-{title:"Tab 3" , content: "Tab 3"} ]
 const App = () => {
+const [Tabs,setTabs]=useState("");
+
+
+function handleClick(e){
+  setTabs(e.target.innerText);
+}
+
+
+
+
+
   return (
     <div>
         {/* Do not remove the main div */}
-        <Tabs tabs={tabs}/>
+        <ul>
+          <li onClick={handleClick}>Tab 1</li>
+          <li onClick={handleClick}>Tab 2</li>
+          <li onClick={handleClick}>Tab 3</li>
+        </ul>
+        <p>This is the content for {Tabs}</p>
     </div>
   )
 }
